0535613bc0ae22755a93c4b205dcc5332d858fff
Jumana-mampalli <jumanaabdulnazer@gmail.com>
2025-11-04 09:59:34 +0530

    Delete api_urls.py

--- api_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import api_views
-
-app_name = 'api'
-
-urlpatterns = [
-    path('bin-status/', api_views.bin_status_update, name='bin_status'),
-    path('bin-status/<str:bin_id>/', api_views.bin_status_detail, name='bin_status_detail'),
-]
